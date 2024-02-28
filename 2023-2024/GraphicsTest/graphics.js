bc12c93e1f4847d8337f248a08e325f11f352aff
BOBBYPENGUINOfficial <89047661+BOBBYPENGUINOfficial@users.noreply.github.com>
2024-02-28 09:18:15 -0700

    Update graphics.js

--- 2023-2024/GraphicsTest/graphics.js
+++ 2023-2024/GraphicsTest/graphics.js
@@ -5,6 +5,14 @@ var Graphics = function(xPos, yPos, scale, type){
     this.type = type;
     RED = 'rbg(255,0,0)'
 }
+Graphics.prototype.getCoordinates = function(){
+    
+    for(var row = 0; row < 16; row++){
+        for(var column = 0; column < 16; column++){
+
+        }
+    }
+}
 Graphics.prototype.drawRedSquare = function(){
     return [[RED]*16]*16;
 }

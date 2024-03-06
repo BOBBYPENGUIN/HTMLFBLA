e36322ccdb5e555814dcdfc9bee953df5d814eec
BOBBYPENGUINOfficial <89047661+BOBBYPENGUINOfficial@users.noreply.github.com>
2024-03-06 14:10:29 -0700

    Update

--- 2023-2024/Java Based/FBLA/src/Game.java
+++ 2023-2024/Java Based/FBLA/src/Game.java
@@ -1,10 +1,15 @@
 package src;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.*;
 
 public class Game {
 
     public static void main(String[] args) {
         var frame = new GameFrame();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize((int) screenSize.getWidth(), (int) screenSize.getHeight());
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }

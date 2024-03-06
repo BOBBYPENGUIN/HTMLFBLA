a260019b23cb98b688fe504e5b46307b3a289b14
BOBBYPENGUINOfficial <89047661+BOBBYPENGUINOfficial@users.noreply.github.com>
2024-03-06 13:28:11 -0700

    More Updates

--- 2023-2024/Java Based/FBLA/GameFrame.java
+++ 2023-2024/Java Based/FBLA/GameFrame.java
@@ -1,5 +1,7 @@
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -7,6 +9,27 @@ import javax.swing.JFrame;
 public class GameFrame extends JFrame{
     private ArrayList<Assets> tiles = new ArrayList<>();
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    class MyKeyListener implements KeyListener {
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+        }
+        
+    }
     public GameFrame(){
         tiles.add(new Assets(5, 5, 5, 1));
         for(var tile : tiles){

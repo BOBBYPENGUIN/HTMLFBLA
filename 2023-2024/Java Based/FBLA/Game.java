74a7ca532e78a1af7cdd8ed973c7177330b95719
BOBBYPENGUINOfficial <89047661+BOBBYPENGUINOfficial@users.noreply.github.com>
2024-03-06 10:27:33 -0700

    Added Game

---
+++ 2023-2024/Java Based/FBLA/Game.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Game {
+
+    public static void main(String[] args) {
+        // var frame = new ItsSnowingApp();
+        // frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        // frame.setVisible(true);
+    }
+}

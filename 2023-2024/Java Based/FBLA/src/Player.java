4ea1b847dc0961aa2a8fe9158490bf84d81cdecd
BOBBYPENGUINOfficial <89047661+BOBBYPENGUINOfficial@users.noreply.github.com>
2024-03-07 10:17:30 -0700

    More

---
+++ 2023-2024/Java Based/FBLA/src/Player.java
@@ -0,0 +1,33 @@
+package src;
+
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+public class Player extends JComponent{
+    class MyKeyListener implements KeyListener {
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+        }
+
+    }
+}

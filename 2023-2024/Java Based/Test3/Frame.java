5ce9e27161a086209ddc751a453ecc933d153e71
Brian Wei <67200110+branwi@users.noreply.github.com>
2024-03-11 11:02:36 -0600

    asdf

--- 2023-2024/Java Based/Test3/Frame.java
+++ 2023-2024/Java Based/Test3/Frame.java
@@ -1,23 +1,39 @@
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.swing.JFrame;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
-public class Frame extends JFrame{
+public class Frame extends JFrame {
     class TimerListener implements ActionListener{
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            
+
         }
-        
+
     }
-    private Person panel = new Person();
+    BufferedImage myPicture;
+
+    {
+        try {
+            myPicture = ImageIO.read(new File("Iowa.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+
+    //private Person panel = new Person();
     public Frame(){
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        add(panel);
+        add(picLabel);
         setSize(screenSize);
     }
 }

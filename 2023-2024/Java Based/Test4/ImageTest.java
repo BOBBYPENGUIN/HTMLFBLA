bc838452d492851a6da78356dae184f2f7fdee29
Brian Wei <67200110+branwi@users.noreply.github.com>
2024-03-11 17:09:54 -0600

    asdf

--- 2023-2024/Java Based/Test4/ImageTest.java
+++ 2023-2024/Java Based/Test4/ImageTest.java
@@ -1,31 +1,47 @@
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class ImageTest {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class ImageTest extends JComponent{
+    private int x, y;
+    private int state = 0;
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     BufferedImage iowa;
     BufferedImage national;
 
+    private int[] iowaMoney = new int[6];
+    private int[] nationalMoney = new int[5];
     {
         try {
             iowa = ImageIO.read(new File("Iowa.png"));
-            nati
+            national = ImageIO.read(new File("national.png"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-    public ImageTest(int x, int y, int state){
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        add(picLabel);
-        setSize(screenSize);
-    }
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    add(picLabel);
-    setSize(screenSize);
+    JLabel iowaLabel = new JLabel(new ImageIcon("Iowa.png"));
+    JLabel nationalLabel = new JLabel(new ImageIcon("national.png"));
+    class myListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(state == 0){
+                if(iowa.getRGB((int)getMousePosition().getX(),(int)getMousePosition().getY()) == (0,0,0)){
 
+                }
+                        getMousePosition())
+            } else {
+                drawMainScene();
+                repaint();
+            }
+        }
+
+    }
 }

f7e922ecc5256d8b2501e13569c192e86c8d3d5f
BOBBYPENGUINOfficial <89047661+BOBBYPENGUINOfficial@users.noreply.github.com>
2024-03-11 16:44:03 -0600

    Update

--- 2023-2024/Java Based/Test4/ButtonTest.java
+++ 2023-2024/Java Based/Test4/ButtonTest.java
@@ -5,28 +5,42 @@ import javax.swing.*;
 
 class ButtonTest extends JFrame{
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    int state = 1;
     int type;
     String text;
+    public int state = 0;
     class myListener implements ActionListener{
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            if(state == 1){
-                drawInstructions();
-                repaint();
-            } else {
-                drawMainScene();
-                repaint();
+            if(e.getSource() instanceof JButton){
+                String butSrcTxt = e.getActionCommand();
+                if(butSrcTxt.equals("Instructions")){
+                    drawInstructions();
+                    repaint();
+                } else if(butSrcTxt.equals("About")){
+                    drawAbout();
+                    repaint();
+                } else if(butSrcTxt.equals("Leaderboard")){
+                    drawLeaderboard();
+                    repaint();
+                } else if(butSrcTxt.equals("Back")){
+                    drawMainScene();
+                    repaint();
+                }
             }
         }
         
     }
     private JPanel panel = new JPanel();
-    private JButton button1 = new JButton("Test1");
+    private JButton buttonEasy = new JButton("Easy");
+    private JButton buttonMedium = new JButton("Medium");
+    private JButton buttonHard = new JButton("Hard");
+    private JButton buttonAbout = new JButton("About");
+    private JButton buttonInstructions = new JButton("Instructions");
+    private JButton buttonLeaderboard = new JButton("Leaderboard");
     private JButton button2 = new JButton("Enter Game");
     private JButton backButton = new JButton("Back");
-    private JLabel title = new JLabel("Title");
+    private JLabel title = new JLabel("Journey", SwingConstants.CENTER);
     private JLabel directions = new JLabel("Lorem ipsum");
     myListener listen = new myListener();
     public ButtonTest(int type, String text){
@@ -38,29 +52,71 @@ class ButtonTest extends JFrame{
         setSize(screenSize);
     }
     public void drawMainScene(){
-        state = 1;
-        panel.remove(backButton);
-        panel.remove(directions);
-        button1.setBackground(new Color(0, 255, 255));
-        button1.setBounds(10, 500, 500, 500);
-        button1.addActionListener(listen);
-        title.setBounds(100, 100, 200, 200);
+        state = 0;
+        panel.removeAll();
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+        title.setText("Journey");
+        buttonEasy.setBounds((int) width / 6, (int) height * 3 / 9, (int) width / 6,  (int) height / 9);
+        buttonEasy.addActionListener(listen);
+        buttonMedium.setBounds((int) (width* 2.5 / 6), (int) height * 3 / 9, (int) width / 6,  (int) height / 9);
+        buttonMedium.addActionListener(listen);
+        buttonHard.setBounds((int) (width* 4 / 6), (int) height * 3 / 9, (int) width / 6,  (int) height / 9);
+        buttonHard.addActionListener(listen);
+        buttonAbout.setBounds((int) width / 6, (int) height * 5 / 9, (int) width / 6,  (int) height / 9);
+        buttonAbout.addActionListener(listen);
+        buttonInstructions.setBounds((int) (width* 2.5 / 6), (int) height * 5 / 9, (int) width / 6,  (int) height / 9);
+        buttonInstructions.addActionListener(listen);
+        buttonLeaderboard.setBounds((int) (width* 4 / 6), (int) height * 5 / 9, (int) width / 6,  (int) height / 9);
+        buttonLeaderboard.addActionListener(listen);
+        title.setBounds(0, 0, (int) screenSize.getWidth(), (int)  screenSize.getHeight()/3);
         title.setFont(new Font("Serif", Font.PLAIN, 50));
         button2.setBounds(550, 500, 500, 500);
-        panel.add(button1); 
-        panel.add(button2);
+        panel.add(buttonEasy); 
+        panel.add(buttonMedium);
+        panel.add(buttonHard);
+        panel.add(buttonAbout); 
+        panel.add(buttonInstructions);
+        panel.add(buttonLeaderboard);
         panel.add(title);
     }
     public void drawInstructions(){
-        state = 2;
-        panel.remove(title);
-        panel.remove(button1);
-        backButton.setBounds(1000, 500, 500, 500);
+        panel.removeAll();
+        backButton.setBounds((int)(screenSize.getWidth()*4/5), (int)(screenSize.getHeight()*7/10), (int)(screenSize.getWidth()*3/20), (int)(screenSize.getWidth()*2/20));
         backButton.addActionListener(listen);
-        directions.setBounds(100, 100, 1000, 100);
-        directions.setFont(new Font("Serif", Font.PLAIN, 50));
+        directions.setBounds(100, (int) screenSize.getHeight()/3, 1000, 100);
+        directions.setText("Press Back");
+        directions.setFont(new Font("Serif", Font.PLAIN, 25));
+        title.setText("Instructions");
         panel.add(backButton);
         panel.add(directions);
+        panel.add(title);
     }
-
+    public void drawAbout(){
+        panel.removeAll();
+        backButton.setBounds((int)(screenSize.getWidth()*4/5), (int)(screenSize.getHeight()*7/10), (int)(screenSize.getWidth()*3/20), (int)(screenSize.getWidth()*2/20));
+        backButton.addActionListener(listen);
+        directions.setBounds(100, (int) screenSize.getHeight()/3, 1000, 100);
+        directions.setText("Created by Alex Liu and Brian Wei");
+        directions.setFont(new Font("Serif", Font.PLAIN, 25));
+        title.setText("About");
+        panel.add(backButton);
+        panel.add(directions);
+        panel.add(title);
+    }
+    public void drawLeaderboard(){
+        panel.removeAll();
+        backButton.setBounds((int)(screenSize.getWidth()*4/5), (int)(screenSize.getHeight()*7/10), (int)(screenSize.getWidth()*3/20), (int)(screenSize.getWidth()*2/20));
+        backButton.addActionListener(listen);
+        directions.setBounds(100, (int) screenSize.getHeight()/3, 1000, 100);
+        directions.setText("1. 13 Minutes, 45 Seconds");
+        directions.setFont(new Font("Serif", Font.PLAIN, 25));
+        title.setText("Leaderboard");
+        panel.add(backButton);
+        panel.add(directions);
+        panel.add(title);
+    }
+    public int getState(){
+        return state;
+    }
 }

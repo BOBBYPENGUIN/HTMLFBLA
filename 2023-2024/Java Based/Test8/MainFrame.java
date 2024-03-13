f2dd2c0905abd18bfdd6caa62f8371cbd6d4fb8e
BOBBYPENGUINOfficial <89047661+BOBBYPENGUINOfficial@users.noreply.github.com>
2024-03-12 22:53:54 -0600

    Update

---
+++ 2023-2024/Java Based/Test8/MainFrame.java
@@ -0,0 +1,196 @@
+import java.awt.Dimension;
+import java.awt.Label;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.sound.midi.MidiDevice.Info;
+import javax.swing.*;
+
+public class MainFrame extends JFrame{
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private ButtonTest homepage;
+    private QuizFramework question;
+    private ImageTest iowaState;
+    private NationalTest nationState;
+    private BossFight iowaShowdown;
+    private boolean initStatus[] = {false};
+    private long questionDelay;
+    private InfoPanel cashLabel;
+    Random rand = new Random();
+    int state = 0;
+    int questionStatus = 0;
+    int cash = 1000000;
+    int difficulty;
+    private MainPanel thePanel;
+    private MainPanel nationalPanel;
+    class myListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(state == 0){
+                if(homepage.getState() != 0){
+                    difficulty = homepage.getState();
+                    initWorld();
+                }
+            } else if(state == 1){
+                if(System.currentTimeMillis()/1000 > questionDelay){
+                    initQuestion();
+                } if(thePanel.background.extras.get(0).x-screenSize.getWidth()/2 +16*5< Math.abs(thePanel.dx) && thePanel.background.extras.get(0).x-screenSize.getWidth()/2 +16*5+5*16*10> Math.abs(thePanel.dx)){
+                    if(thePanel.background.extras.get(0).y-screenSize.getWidth()/2 +3*16*5< Math.abs(thePanel.dy) && thePanel.background.extras.get(0).y-screenSize.getWidth()/2 +2*16*5+5*16*10> Math.abs(thePanel.dy)){
+                        initIowaMoney();
+                    }
+                } 
+            } else if(state == 2){
+                if(question.state != 1){
+                    if(question.state == 2){
+                        cash = cash + 1000 + Math.abs(Math.abs(thePanel.dx)-Math.abs(600)) + Math.abs(Math.abs(thePanel.dy)-Math.abs(880));
+                    }
+                    repaint();
+                    try {
+                        Thread.sleep(2500);
+                    } catch (InterruptedException b) {
+                        b.printStackTrace();
+                    }
+                    state = 3;
+                    drawCashLabel();
+
+                }
+            } else if(state == 3){
+                try {
+                    Thread.sleep(2500);
+                } catch (InterruptedException b) {
+                    b.printStackTrace();
+                }
+                initWorld();
+            } else if(state == 4){
+                if(iowaState.state == -1){
+                    initIowaShowdown();
+                }
+            } else if(state == 5){
+                if(iowaShowdown.done){
+                    try {
+                        Thread.sleep(2500);
+                    } catch (InterruptedException b) {
+                        b.printStackTrace();
+                    }
+                    nationalPanel = new MainPanel();
+                    initNationalWorld();
+                }
+            } else if(state == 6){
+                if(System.currentTimeMillis()/1000 > questionDelay){
+                    initQuestion();
+                } if(thePanel.background.extras.get(0).x-screenSize.getWidth()/2 +16*5< Math.abs(thePanel.dx) && thePanel.background.extras.get(0).x-screenSize.getWidth()/2 +16*5+5*16*10> Math.abs(thePanel.dx)){
+                    if(thePanel.background.extras.get(0).y-screenSize.getWidth()/2 +3*16*5< Math.abs(thePanel.dy) && thePanel.background.extras.get(0).y-screenSize.getWidth()/2 +2*16*5+5*16*10> Math.abs(thePanel.dy)){
+                        System.out.println("National");
+                        initNationMoney();
+                    }
+                } 
+            } else if(state == 7){
+                try {
+                    Thread.sleep(2500);
+                } catch (InterruptedException b) {
+                    b.printStackTrace();
+                }
+                initNationalWorld();
+            } else if(state == 8){
+                if(nationState.state == -1){
+                    initNationShowdown();
+                }
+            }
+            repaint();
+        }
+        
+    }
+    public MainFrame(){
+        homepage = new ButtonTest(0, "0");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(screenSize);
+        thePanel = new MainPanel();
+        //setLayout(null);
+        //add(thePanel);
+        add(homepage);
+        myListener listener = new myListener();
+        Timer timer = new Timer(1, listener);
+        timer.start();
+    }
+    public void initWorld(){
+        state = 1;
+        //removeAll();
+        setContentPane(thePanel);
+        revalidate();
+        thePanel.addKeyListener();
+        repaint();
+        questionDelay = rand.nextInt(5)+ 5 + (long) System.currentTimeMillis()/1000;
+        //thePanel.addKeyListener(thePanel.theKeyListener);
+    }
+    public void initQuestion(){
+        state = 2;
+        thePanel.keyIsPressed = false;
+        for(var i = 0; i < thePanel.keyStatus.length; i++){
+            thePanel.keyStatus[i] = false;
+        }
+        question = new QuizFramework();
+        setContentPane(question);
+        revalidate();
+        repaint(); 
+    }
+    public void initNationalQuestion(){
+        state = 7;
+        thePanel.keyIsPressed = false;
+        for(var i = 0; i < thePanel.keyStatus.length; i++){
+            thePanel.keyStatus[i] = false;
+        }
+        question = new QuizFramework();
+        setContentPane(question);
+        revalidate();
+        repaint(); 
+    }
+    public void drawCashLabel(){
+        cashLabel = new InfoPanel("You currently have: $" + Integer.toString(cash));
+        setContentPane(cashLabel);
+        revalidate();
+        repaint(); 
+    }
+    public void initIowaMoney(){
+        state = 4;
+        iowaState = new ImageTest(cash);
+        setContentPane(iowaState);
+        revalidate();
+        repaint(); 
+    }
+    public void initNationMoney(){
+        state = 8;
+        nationState = new NationalTest(cash);
+        setContentPane(nationState);
+        revalidate();
+        repaint(); 
+    }
+    public void initIowaShowdown(){
+        state = 5;
+        iowaShowdown = new BossFight(difficulty, iowaState.getMoney());
+        setContentPane(iowaShowdown);
+        revalidate();
+        repaint(); 
+    }
+    public void initNationalWorld(){
+        state = 6;
+        //removeAll();
+        setContentPane(nationalPanel);
+        revalidate();
+        nationalPanel.addKeyListener();
+        repaint();
+        questionDelay = rand.nextInt(5)+ 5 + (long) System.currentTimeMillis()/1000;
+        //thePanel.addKeyListener(thePanel.theKeyListener);
+    }
+    public void initNationShowdown(){
+        state = 8;
+        iowaShowdown = new BossFight(difficulty, iowaState.getMoney());
+        setContentPane(iowaShowdown);
+        revalidate();
+        repaint(); 
+    }
+}
---
+++ 2023-2024/Java Based/Test8/QuizFramework.java
@@ -0,0 +1,106 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+
+import javax.swing.*;
+
+class QuizFramework extends JPanel{
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    int state = 1;
+    String text;
+    private int questionNum;
+    class myListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String questionSelected  = e.getActionCommand();
+            if(questionSelected.equals(answers.get(questionNum).get(realAnswers.get(questionNum)))){
+                state = 2;
+            } else {
+                state = 3;
+            }
+            drawAnswer();
+        }
+        
+    }
+    ArrayList<String> questions = new ArrayList<>();
+    ArrayList<ArrayList<String>> answers = new ArrayList<ArrayList<String>>();
+    ArrayList<Integer> realAnswers = new ArrayList<>();
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JLabel question;
+    private JLabel result;
+    myListener listen = new myListener();
+    public QuizFramework(){
+        addQuestions();
+        questionNum = getRandomQuestion();
+        button1 = new JButton(answers.get(questionNum).get(0));
+        button2 = new JButton(answers.get(questionNum).get(1));
+        button3 = new JButton(answers.get(questionNum).get(2));
+        button4 = new JButton(answers.get(questionNum).get(3));
+        question = new JLabel(questions.get(questionNum), SwingConstants.CENTER);
+        result = new JLabel("Correct!", SwingConstants.CENTER);
+        setLayout(null);
+        drawQuestion();
+        setSize(screenSize);
+    }
+    public void drawQuestion(){
+        removeAll();
+        state = 1;
+        question.setBounds((int) screenSize.getWidth()/5, (int) screenSize.getHeight ()/10, (int) screenSize.getWidth()*3/5, (int) screenSize.getHeight()/5);
+        question.setFont(new Font("Serif", Font.PLAIN, 50));
+        add(question);
+        button1.setBounds((int) screenSize.getWidth()/10, (int) screenSize.getHeight ()/3, (int) screenSize.getWidth()*3/10, (int) screenSize.getHeight()/5);
+        button1.addActionListener(listen);
+        add(button1);
+        button2.setBounds((int) screenSize.getWidth()*6/10, (int) screenSize.getHeight ()/3, (int) screenSize.getWidth()*3/10, (int) screenSize.getHeight()/5);
+        button2.addActionListener(listen);
+        add(button2);
+        button3.setBounds((int) screenSize.getWidth()/10, (int) (screenSize.getHeight()*2/3), (int) screenSize.getWidth()*3/10, (int) screenSize.getHeight()/5);
+        button3.addActionListener(listen);
+        add(button3);
+        button4.setBounds((int) screenSize.getWidth()*6/10, (int) (screenSize.getHeight ()*2/3), (int) screenSize.getWidth()*3/10, (int) screenSize.getHeight()/5);
+        button4.addActionListener(listen);
+        add(button4);
+    }
+    public void addQuestion(String question, ArrayList<String> answers, int realAnswer){
+        questions.add(question);
+        this.answers.add(answers);
+        this.realAnswers.add(realAnswer);
+    }
+    public void addQuestion(String question, String a1, String a2, String a3, String a4, int realAnswer){
+        questions.add(question);
+        ArrayList<String> tempAns = new ArrayList<>();
+        tempAns.add(a1);
+        tempAns.add(a2);
+        tempAns.add(a3);
+        tempAns.add(a4);
+        this.answers.add(tempAns);
+        this.realAnswers.add(realAnswer);
+    }
+    public void addQuestions(){
+        addQuestion("When was FBLA founded?", "2020", "1999", "1956", "1937", 3);
+        addQuestion("Is the letter A:", "A", "B", "C", "D", 0);
+    }
+    public int getRandomQuestion(){
+        Random rand = new Random();
+        return rand.nextInt(questions.size());
+    }
+    public void drawAnswer(){
+        removeAll();
+        result.setBounds((int) screenSize.getWidth()/5, (int) screenSize.getHeight ()/10, (int) screenSize.getWidth()*3/5, (int) screenSize.getHeight()/5);
+        result.setFont(new Font("Serif", Font.PLAIN, 50));
+        if(state == 2){
+            result.setText("Correct!");
+        } else {
+            result.setText("Incorrect. The answer is " + answers.get(questionNum).get(realAnswers.get(questionNum)));
+        }
+        //System.out.println(result.getText());
+        add(result);
+        repaint();
+    }
+
+}
